12fc1907a44d96ebe301d5238933be4a3f6927df
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-31 00:38:19 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -65,12 +65,13 @@ end
 def runner
   welcome
   card_total = initial_round
-  hit?(card_total)
-  display_card_total(card_total)
-  new_card_total = hit?(card_total)
-  until card_total > 21
-    end_game(card_total)
+    until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
+
+end_game(card_total)
+
   
 end
     
